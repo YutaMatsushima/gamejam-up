56af9080e48b3265a9efdb7b9914c540e1f561f0
YutaMatsushima <y-mats.en-312@moegi.waseda.jp>
2019-12-27 14:54:41 +0900

    プレイヤーの動きとステージ作成

---
+++ UpGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        transform.position = new Vector3(transform.position.x, playerPos.y, transform.position.z);
+    }
+}
---
+++ UpGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public const int MAX_JUMP_COUNT = 2;
+    public float jumpforce = 5.0f;
+    private Vector3 side = new Vector3(0.1f, 0f, 0f);
+    private int jumpcount = 0;
+    private bool isJump = false;
+    void start()
+    {
+        this.gameObject.transform.position = new Vector3(-18.0f, -8.0f, 0.0f);
+    }
+    void Update()
+    {
+        if(jumpcount < MAX_JUMP_COUNT && Input.GetKeyDown(KeyCode.W)){
+            isJump = true;
+        }   
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = this.transform.GetComponent<Rigidbody>();
+        Vector3 now = rb.position;
+        if(isJump){
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            jumpcount++;
+            isJump = false;
+
+        }
+        if(Input.GetKey(KeyCode.A)){
+            now += side * -1;
+            rb.position = now;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            now += side * 1;
+            rb.position = now;
+        }
+        if(now.y < -10.0f){
+            Debug.Log("game over");
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "floor"){
+            jumpcount = 0;
+        }
+    }
+}
